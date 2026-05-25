e9b0fb7d1de643ba4fc1a26a12944ffc688042ca
Anwesh Budhathoki <abperni@gmail.com>
2026-05-25 19:26:10 +0545

    Update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 prompt_install() {
   local cmd="$1"
   local pkg="$2"
-  read -r "reply?Install $pkg? [y/N] "
+  read -p "Install $2: [y/N] " reply
+  # read -r "reply?Install $pkg? [y/N] "
   if [[ "$reply" =~ ^[Yy]$ ]]; then
     brew install $cmd "$pkg"
   fi
+  echo ""
+  echo ""
 }
 
 # Core
